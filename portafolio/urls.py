06fc33e59a58e617ac3a5f45e960a969a59f7ed8
william0814 <king-master90@hotmail.com>
2025-01-25 05:16:03 +0100

    templates and styles 1

--- portafolio/urls.py
+++ portafolio/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about-me/', views.about_me, name='about_me'),
+    path('studies/', views.studies, name='studies'),
+    path('experience/', views.experience, name='experience'),
 
 ]
--- portafolio/views.py
+++ portafolio/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'portafolio/home.html')
+
+def about_me(request):
+    return render(request, 'portafolio/about_me.html')
+
+def studies(request):
+    return render(request, 'portafolio/studies.html')
+
+def experience(request):
+    return render(request, 'portafolio/experience.html')
