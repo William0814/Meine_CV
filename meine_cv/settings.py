368bd2870c7a6b04e4c9504a3df250cb31632e02
william0814 <king-master90@hotmail.com>
2025-02-02 22:09:37 +0100

    6 attempt deploy

--- meine_cv/settings.py
+++ meine_cv/settings.py
@@ -80,9 +80,10 @@ WSGI_APPLICATION = "meine_cv.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": 
-        dj_database_url.config(default=os.getenv('DATABASE_URL'))
-
+    "default":
+    dj_database_url.config(default=os.getenv('DATABASE_URL')),
+       
+    
     }
 
 
