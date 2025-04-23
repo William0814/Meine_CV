78ed11f2232c24d9f24ec143f1d11bfafb0cbde5
william0814 <king-master90@hotmail.com>
2025-04-23 13:39:30 +0200

    Change CV,pdf

--- portafolio/admin.py
+++ portafolio/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import Contact
 
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'message', 'created_at')
-    search_fields = ('name', 'email')
-    list_filter = ('created_at',)
+
+# class ContactAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'email', 'message', 'created_at')
+#     search_fields = ('name', 'email')
+#     list_filter = ('created_at',)
 
 
-admin.site.register(Contact, ContactAdmin)
+admin.site.register(Contact)
 # Register your models here.
