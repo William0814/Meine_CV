dcd5a2bee8a0d4411f6eba4a602aa5dd842a3ecd
william0814 <king-master90@hotmail.com>
2025-08-08 17:53:17 +0200

    Update database settings for DEBUG mode and fix typo in studies.html

--- meine_cv/settings.py
+++ meine_cv/settings.py
@@ -83,10 +83,13 @@ WSGI_APPLICATION = "meine_cv.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-    }
+if os.getenv("DEBUG") == "True":
+    DATABASES = {
+        'default':{
+            'ENGINE':'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        } 
+}
 
 
     
